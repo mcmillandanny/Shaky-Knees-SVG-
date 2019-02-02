0c49d864a4630d67f5f4324dd3fb2f00ce1b2cef
Dnny <mcmillandanny@gmail.com>
2019-02-02 12:57:47 -0500

    adding in loading animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,37 @@
-"use strict";console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+"use strict";
+
+console.log('Shaky Knees');
+
+var incubusAnimateIn = new TimelineMax({ delay: 1.5 });
+var tameAnimateIn = new TimelineMax({});
+var beckAnimateIn = new TimelineMax({ delay: 1.3 });
+var cageAnimateIn = new TimelineMax({ delay: 1.9 });
+
+var otherBandsAnimatein = new TimelineMax({ delay: 4 });
+
+tameAnimateIn.from("#Tame_Impala g path", 1, {
+    y: "-400px",
+    scale: .1
+});
+
+beckAnimateIn.to("#BECK", 1, {
+    opacity: 1
+
+});
+
+beckAnimateIn.from("#BECK", 1, {
+    scale: 1.25
+});
+
+incubusAnimateIn.from("#INCUBUS g path", 2, {
+    x: "900px"
+});
+
+cageAnimateIn.from("#cagetheelephant g path", 2, {
+    y: "900px"
+});
+
+otherBandsAnimatein.to("#maggierogers", 1.5, {
+    opacity: 1
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
 console.log('Shaky Knees')
+
+
+var incubusAnimateIn = new TimelineMax({delay: 1.5});
+var tameAnimateIn = new TimelineMax({});
+var beckAnimateIn = new TimelineMax({delay: 1.3});
+var cageAnimateIn = new TimelineMax({delay: 1.9});
+
+var otherBandsAnimatein = new TimelineMax({delay: 4});
+
+tameAnimateIn.from("#Tame_Impala g path", 1, {
+    y: "-400px", 
+    scale: .1
+});
+
+beckAnimateIn.to("#BECK", 1, {
+    opacity: 1, 
+
+});
+
+beckAnimateIn.from("#BECK", 1, {
+    scale: 1.25 
+});
+
+incubusAnimateIn.from("#INCUBUS g path", 2 ,{
+    x: "900px"
+})
+
+cageAnimateIn.from("#cagetheelephant g path", 2 ,{
+    y: "900px"
+})
+
+otherBandsAnimatein.to("#maggierogers", 1.5, {
+    opacity: 1
+});
+
