6ceaf7a6375aab17aac4df6ffc4eb6f81712c78b
Dnny <mcmillandanny@gmail.com>
2019-02-02 17:46:33 -0500

    styling the home page

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ var cageAnimateIn = new TimelineMax({delay: 1.9});
 
 var otherBandsAnimatein = new TimelineMax({delay: 4});
 
-tameAnimateIn.from("#Tame_Impala g path", 1, {
+tameAnimateIn.from("#Tame_Impala g path", .7, {
     y: "-400px", 
     scale: .1
 });
@@ -22,7 +22,7 @@ beckAnimateIn.from("#BECK", 1, {
     scale: 1.25 
 });
 
-incubusAnimateIn.from("#INCUBUS g path", 2 ,{
+incubusAnimateIn.from("#INCUBUS g path", .8 ,{
     x: "900px"
 })
 
@@ -30,7 +30,38 @@ cageAnimateIn.from("#cagetheelephant g path", 2 ,{
     y: "900px"
 })
 
-otherBandsAnimatein.to("#maggierogers", 1.5, {
+otherBandsAnimatein.to("#maggierogers", .5, {
     opacity: 1
 });
+
+otherBandsAnimatein.to("#caamp", .5, {
+    opacity: 1
+});
+
+otherBandsAnimatein.to("#jimjames", .5, {
+    opacity: 1
+});
+
+otherBandsAnimatein.to("#foals", .5, {
+    opacity: 1
+});
+
+otherBandsAnimatein.to("#grouplove", .5, {
+    opacity: 1
+});
+
+otherBandsAnimatein.to("#thestruts", .5, {
+    opacity: 1
+});
+
+otherBandsAnimatein.to("#interpol", .5, {
+    opacity: 1
+});
+
+
+var day1Btn = document.querySelector('.day1-btn');
+
+console.log(day1Btn);
+
+
 
